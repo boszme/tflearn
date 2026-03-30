0bb66bc5764e402405c73cb3660e7e4dc905650f
boszme <dddrue@gmail.com>
2026-03-30 12:47:05 +0200

    first test ec2

---
+++ first_ec2.tf
@@ -0,0 +1,10 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-north-1"
+}
+
+# Create a new host with instance type of c5.18xlarge with Auto Placement
+# and Host Recovery enabled.
+resource "aws_ec2_host" "test" {
+  instance_type     = "t2.micro"
+}
